99bc184940305d69f09ed2fae1bd073743120127
G <g@brabyn.info>
2020-10-06 17:37:56 +1300

    Laravel Dusk - browser test set up

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\TestCase as BaseTestCase;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        // static::startChromeDriver();
+    }
+
+    protected function driver()
+    {
+        $options = (new ChromeOptions)->addArguments([
+            '--disable-gpu',
+            '--headless'
+        ]);
+
+        return RemoteWebDriver::create(
+            'http://selenium-hub:4444/wd/hub',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY_W3C,
+                $options
+            )
+        );
+    }
+}
